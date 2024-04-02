fdd6f69d9f3008191801a7031a8454f09a26101a
joaocarloslima <joaocarloslima@me.com>
2024-04-02 20:55:01 -0300

    navegação

--- src/app/movimentacoes/page.tsx
+++ src/app/movimentacoes/page.tsx
@@ -1,10 +1,11 @@
 import NavBar from "@/components/NavBar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <NavBar />
-      <h2>Dashboard</h2>
+      <h2>Movimentações</h2>
     </main>
   );
 }
