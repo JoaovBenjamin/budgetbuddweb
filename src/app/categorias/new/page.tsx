20a72a6b167df03690f86bc72391e79de7b0ca8a
joaocarloslima <joaocarloslima@me.com>
2024-04-15 22:06:45 -0300

    cadastro e validação de categoria

--- src/app/categorias/new/page.tsx
+++ src/app/categorias/new/page.tsx
@@ -1,50 +1,53 @@
+"use client"
+
+import { create } from "@/app/actions/categorias/create";
+import { icons } from "@/app/utils/icons";
 import NavBar from "@/components/NavBar";
-import { Button, Input } from "@nextui-org/react";
+import { SubmitButton } from "@/components/SubmitButton";
+import { Autocomplete, AutocompleteItem, Button, Input } from "@nextui-org/react";
 import { ArrowLeft, Check } from "lucide-react";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import { useFormState } from "react-dom";
 
-export default async function FormCategorias() {
-
-  async function create(formData: FormData){
-    "use server"
-    const data = {
-      nome: formData.get("nome"),
-      icone: "book"
-    }
-
-    const options = {
-      method: "post",
-      body: JSON.stringify(data),
-      headers:{
-        "Content-Type": "application/json"
-      }
-    }
-
-    fetch("http:localhost:8080/categoria", options)
-
-    redirect("/categorias")
+export default function FormCategorias() {
+  const initialState = {
+    message: ""
   }
 
+  const [state, formAction] = useFormState(create, initialState)
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <NavBar active="categorias"/>
+      <NavBar active="categorias" />
 
-      <form action={create} className="flex flex-col bg-slate-900 p-6 m-6 min-w-[500px] rounded">
+      <form action={formAction} className="flex flex-col gap-4 bg-slate-900 p-6 m-6 min-w-[500px] rounded">
         <h2 className="text-2xl font-bold">Cadastrar Categoria</h2>
         <Input
-              key="nome"
-              label="Nome"
-              name="nome"
-              labelPlacement="outside"
+          key="nome"
+          label="Nome"
+          name="nome"
+          variant="bordered"
+          labelPlacement="outside"
+          isInvalid={state?.message != ""}
+          errorMessage={state?.message}
         />
+
+        <Autocomplete
+          label="Ícone"
+          placeholder="procurar ícone..."
+          variant="bordered"
+          labelPlacement="outside"
+          name="icone"
+          defaultItems={icons}
+        >
+          {(item) => <AutocompleteItem key={item.name} startContent={item.icon} >{item.name}</AutocompleteItem>}
+        </Autocomplete>
 
         <div className="flex justify-around mt-4">
           <Link href="/categorias">
             <Button variant="bordered" startContent={<ArrowLeft size={18} />}>cancelar</Button>
           </Link>
 
-          <Button type="submit" color="primary" startContent={<Check size={18} />}>salvar</Button>
+          <SubmitButton />
         </div>
       </form>
     </main>
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,22 +1,11 @@
-import { Apple, Book, Bus, Cigarette } from "lucide-react";
+import { icons } from "@/app/utils/icons"
 
 interface IconProps {
     name: string
 }
 
-interface IconsType {
-    [key: string]: JSX.Element
-}
-
-const icons : IconsType = {
-    apple: <Apple />,
-    bus: <Bus />,
-    cigarette: <Cigarette />,
-    book: <Book />
-}
-
 export function Icon( { name }: IconProps){
 
-    return icons[name]
+    return icons.find(icon => icon.name == name)?.icon
     
 }
