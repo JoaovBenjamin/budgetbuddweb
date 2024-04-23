ee71705929f61a698c859ebef9efbfaecb714670
joaocarloslima <joaocarloslima@me.com>
2024-04-22 22:40:34 -0300

    remove delay

--- src/app/actions/movimentacoes/get.ts
+++ src/app/actions/movimentacoes/get.ts
@@ -3,5 +3,6 @@
 export async function get() {
     await new Promise(r => setTimeout(r, 1000))
     const resp = await fetch("http:localhost:8080/movimentacao", { next: { revalidate: 0 } })
-    return await resp.json()
+    const json = await resp.json()
+    return json.content
 }
